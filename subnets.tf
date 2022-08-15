12d608c4d0d93ecb2d8c9ccf16627689e59ff490
aliya ovad <aliya.ovad.2015@gmail.com>
2022-08-15 15:42:46 +0000

    add terrafrom

---
+++ subnets.tf
@@ -0,0 +1,32 @@
+resource "aws_subnet" "sub-public-1" {
+   vpc_id = aws_vpc.vpc-main.id
+   availability_zone =  "eu-central-1a"
+   cidr_block = cidrsubnet(var.vpc_cidr,8,1)
+tags = {
+    Name = "sub-public-1"
+  }
+}
+resource "aws_subnet" "sub-public-2" {
+   vpc_id = aws_vpc.vpc-main.id
+   availability_zone =  "eu-central-1b"
+   cidr_block = cidrsubnet(var.vpc_cidr,8,2)
+tags = {
+    Name = "sub-public-2"
+  }
+}
+resource "aws_subnet" "sub-private-1" {
+   vpc_id = aws_vpc.vpc-main.id
+   availability_zone =  "eu-central-1a"
+   cidr_block = cidrsubnet(var.vpc_cidr,8,3)
+tags = {
+    Name = "sub-private-1"
+  }
+}
+resource "aws_subnet" "sub-private-2" {
+   vpc_id = aws_vpc.vpc-main.id
+   availability_zone =  "eu-central-1b"
+   cidr_block = cidrsubnet(var.vpc_cidr,8,4)
+tags = {
+    Name = "sub-private-2"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "vpc-main" {
+  cidr_block = var.vpc_cidr
+ tags = {
+   Name= "vpc-main"
+  }
+}
+
+
